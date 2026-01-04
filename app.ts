30cd3c35b90ad15ee666e6f1c36abfcdf81af45b
gauravsgc <pgauravpandeydit@gmail.com>
2026-01-04 20:26:31 +0530

    second  change

--- app.ts
+++ app.ts
@@ -2,7 +2,7 @@ import express from "express";
 import type { Request, Response } from "express";
 
 const app = express();
-const PORT=3000;
+const PORT=Number(process.env.PORT)|3000;
 app.get("/", (req: Request, res: Response) => {
  res.status(200).json({
   success: true,
