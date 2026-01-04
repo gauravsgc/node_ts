cc5480d9f43075c275a42a3c446feac7c931c749
gauravsgc <pgauravpandeydit@gmail.com>
2026-01-04 19:04:38 +0530

    first change

--- app.ts
+++ app.ts
@@ -1,1 +1,16 @@
-console.log("radhe radhe");
+import express from "express";
+import type { Request, Response } from "express";
+
+const app = express();
+const PORT=3000;
+app.get("/", (req: Request, res: Response) => {
+ res.status(200).json({
+  success: true,
+  data: { name: "radhe" }
+});
+
+});
+
+app.listen(PORT, () => {
+  console.log("Server running on http://localhost:3000");
+});
